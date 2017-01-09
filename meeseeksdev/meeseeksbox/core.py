e6f2c61e1e8e792eee09824d97086841c1925409
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-01-09 12:45:31 +0100

    ignore deployment and deployment status

--- meeseeksdev/meeseeksbox/core.py
+++ meeseeksdev/meeseeksbox/core.py
@@ -118,7 +118,7 @@ class WebHookHandler(MainHandler):
         else:
             event_type = self.request.headers.get('X-GitHub-Event')
 
-            if event_type in {'status','fork'}:
+            if event_type in {'status','fork','deployment_status', 'deployment'}:
                 print('Not handeling event type', event_type,'yet.')
                 return self.finish()
 
