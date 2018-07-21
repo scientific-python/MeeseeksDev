319092e10f41c9f6dbf53681972920eaac32d044
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-07-20 18:44:14 -0700

    Indent messages

--- meeseeksdev/meeseeksbox/core.py
+++ meeseeksdev/meeseeksbox/core.py
@@ -202,8 +202,9 @@ class WebHookHandler(MainHandler):
                 if self.mention_bot_re.findall(body):
                     self.dispatch_on_mention(body, payload, user)
                 else:
+                    import textwrap
                     print(f'({repo}) Was not mentioned, (',
-                          self.config.botname,')', body, '|', user)
+                          self.config.botname,')\n', textwrap.indent(body, '   |') , 'by |', user)
             elif installation and installation.get('account'):
                 print(f'({repo}) we got a new installation.')
                 self.auth._build_auth_id_mapping()
