dba4c18ebc650103d792d5a7556821ce41841616
Steven Silvester <steven.silvester@ieee.org>
2022-03-23 05:26:35 -0500

    add gha and flake8

---
+++ .github/workflows/connect.py
@@ -0,0 +1,23 @@
+import errno
+import time
+from urllib import request
+from urllib.error import URLError
+
+t0 = time.time()
+found = False
+url = 'http://localhost:9874'
+
+while (time.time() - t0) < 60:
+    try:
+        request.urlopen(url)
+        found = True
+        break
+    except URLError as e:
+        if e.reason.errno == errno.ECONNREFUSED:
+            time.sleep(1)
+            continue
+        raise
+
+
+if not found:
+    raise ValueError(f'Could not connect to {url}')
